aab778b24ca5fcbda4915a5300bff5b52317237b
Leo <leonidivanovzalgo@gmail.com>
2026-06-10 20:46:12 +0200

    changed script again

--- src/pull-models.sh
+++ src/pull-models.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-pip install -r requirements.txt
+# pip install -r requirements.txt
 pip install huggingface_hub
 
 # pull model weights
 mkdir -p models
-huggingface-cli download leo7d3/unet unet_20260609_150324_n52_lr0.0001_f64.pt --local-dir models/
-huggingface-cli download leo7d3/unet cbam_20260609_162153_n52_lr0.0001_f64.pt --local-dir models/
+hf download leo7d3/unet unet_20260609_150324_n52_lr0.0001_f64.pt --local-dir models/
+hf download leo7d3/unet cbam_20260609_162153_n52_lr0.0001_f64.pt --local-dir models/
